9f8b8f818c6795d06726fe9dc6f3b9aa6b05befb
Brian McPhillips <B.McPhillips89@gmail.com>
2021-02-03 16:22:53 -0800

    import DOM elements, set placeholder transactions to state, transaction to DOM function and init function

---
+++ script.js
@@ -0,0 +1,39 @@
+// Import DOM Elements
+const balance = document.getElementById('balance'),
+  moneyPlus = document.getElementById('money-plus'),
+  moneyMinus = document.getElementById('money-minus'),
+  list = document.getElementById('list'),
+  form = document.getElementById('form'),
+  text = document.getElementById('text'),
+  amount = document.getElementById('amount');
+
+const dummyTransactions = [
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+];
+
+let transactions = dummyTransactions;
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
+    </span> <button class ="delete-btn">x</button>
+    `;
+  list.appendChild(item);
+}
+
+function init() {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+}
+
+init();
